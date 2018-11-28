08e50c4e4a29abf7935992bb00183aa920cc569b
Ryan <tiangao0611@gmail.com>
2018-11-27 22:07:31 -0800

    final edition

--- findcountry.R
+++ findcountry.R
@@ -3,20 +3,22 @@
 suppressPackageStartupMessages(library(tidyverse))
 gapminder <- read.table(file = './data/gapminder.tsv', sep = '\t', header = TRUE)
 
-
+# first read data
 words <- readLines("./data/words.txt") %>% 
 	as.data.frame()
 names(words) <- c("Words")
 a  = words[words$Words, ]
 b = vector()
 
+# filter the data 
+# if the length is shorter than 5
 for(i in a){
 	if(nchar(i)<=5){
 		b = c(b,i)
 	}
 }
 
-
+# filter the gapminder against the previously filtered data
 gapminder_by_words <- gapminder %>% 
 	filter(country %in% b) %>% 
 	group_by(country) %>% 
